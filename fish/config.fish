b772f911fcaf92e854d55a9dce9a150fbb8ac852
David Ochoa <ochoasabogaldavid@gmail.com>
2025-04-08 22:30:37 -0500

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+alias c.="cd .."
+alias SSID="nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2"
+alias z="clear && fish"
+alias ki="kill -9 -1"
+alias na="nano -0 -l"
+alias up="sudo pacman -Syu"
+alias s="clear && su"
+alias shut='shutdown now'
+alias rmh='rm /home/Aphroth/fish/fish_history'
+alias edi='kwrite'
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
