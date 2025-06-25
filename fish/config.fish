0f37f4b2a819853f33dc11755ac29dbebb6d833b
David Ochoa <ochoasabogaldavid@gmail.com>
2025-06-25 18:22:49 -0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,19 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
+set -U fish_greeting
+export PATH="$HOME/.local/bin:$PATH"
 alias c.="cd .."
 alias SSID="nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2"
 alias z="clear && fish"
 alias ki="kill -9 -1"
 alias na="nano -0 -l"
 alias l="ls -la"
+#alias dn="sudo dnf5"
+#alias up="dn update -y && dn upgrade -y"
 alias up="sudo pacman -Syu && yay -Syu"
 alias s="clear && su"
 alias shut='shutdown now'
-alias rmh='rm /home/Aphroth/fish/fish_history'
+alias rmh='rm ~/fish/fish_history'
 alias edi='kwrite'
 wal -R -n
 clear
+sleep 0.1
 fastfetch
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
